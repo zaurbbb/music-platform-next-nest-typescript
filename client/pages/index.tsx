85af0f0f5f50bf896bf856c60b6905b1ccae360f
zaurbbb <zaurberdibekov@mail.ru>
2023-08-02 22:01:57 +0600

    Initialize Next application

---
+++ client/pages/index.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+import React, { FC } from "react";
+
+const Index: FC = () => {
+  return (
+    <div>
+      <Button>asdads</Button>
+    </div>
+  );
+};
+
+export default Index;
+
